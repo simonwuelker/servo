366fd7c8eeb0ac169a2379dc871ee6848c4ac53f
Simon Wülker <simon.wuelker@arcor.de>
2025-02-17 14:57:00 +0100

    Add SVGCircleElement webidl interface
    All members are currently commented out, this is just
    a stub.
    
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

---
+++ components/script/dom/svggeometryelement.rs
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+use dom_struct::dom_struct;
+use html5ever::{LocalName, Prefix};
+
+use crate::dom::svggraphicselement::SVGGraphicsElement;
+use crate::dom::document::Document;
+
+#[dom_struct]
+pub(crate) struct SVGGeometryElement {
+    svggraphicselement: SVGGraphicsElement,
+}
+
+impl SVGGeometryElement {
+    pub(crate) fn new_inherited(
+        tag_name: LocalName,
+        prefix: Option<Prefix>,
+        document: &Document,
+    ) -> SVGGeometryElement {
+        SVGGeometryElement { svggraphicselement: SVGGraphicsElement::new_inherited(tag_name, prefix, document) }
+    }
+}
