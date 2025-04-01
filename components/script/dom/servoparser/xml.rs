b2444eff1ac4e8cdabe7cc277c219a47ebdddfe4
Simon Wülker <simon.wuelker@arcor.de>
2025-04-01 12:21:43 +0200

    todo

--- components/script/dom/servoparser/xml.rs
+++ components/script/dom/servoparser/xml.rs
@@ -6,10 +6,10 @@
 
 use std::cell::Cell;
 
-use markup5ever::TokenizerResult;
+use markup5ever::{DecodingParser, ParserAction};
 use script_bindings::trace::CustomTraceable;
 use servo_url::ServoUrl;
-use xml5ever::buffer_queue::BufferQueue;
+use tendril::StrTendril;
 use xml5ever::tokenizer::XmlTokenizer;
 use xml5ever::tree_builder::XmlTreeBuilder;
 
@@ -24,7 +24,8 @@ use crate::dom::servoparser::{ParsingAlgorithm, Sink};
 #[cfg_attr(crown, crown::unrooted_must_root_lint::must_root)]
 pub(crate) struct Tokenizer {
     #[ignore_malloc_size_of = "Defined in xml5ever"]
-    inner: XmlTokenizer<XmlTreeBuilder<Dom<Node>, Sink>>,
+    #[no_trace] // TODO
+    inner: DecodingParser<XmlTokenizer<XmlTreeBuilder<Dom<Node>, Sink>>>,
 }
 
 impl Tokenizer {
@@ -37,32 +38,63 @@ impl Tokenizer {
             parsing_algorithm: ParsingAlgorithm::Normal,
         };
 
-        let tb = XmlTreeBuilder::new(sink, Default::default());
-        let tok = XmlTokenizer::new(tb, Default::default());
+        let tree_builder = XmlTreeBuilder::new(sink, Default::default());
+        let tokenizer = XmlTokenizer::new(tree_builder, Default::default());
 
-        Tokenizer { inner: tok }
-    }
-
-    pub(crate) fn feed(&self, input: &BufferQueue) -> TokenizerResult<DomRoot<HTMLScriptElement>> {
-        loop {
-            match self.inner.run(input) {
-                TokenizerResult::Done => return TokenizerResult::Done,
-                TokenizerResult::Script(handle) => {
-                    // Apparently the parser can sometimes create <script> elements without a namespace, resulting
-                    // in them not being HTMLScriptElements.
-                    if let Some(script) = handle.downcast::<HTMLScriptElement>() {
-                        return TokenizerResult::Script(DomRoot::from_ref(script));
-                    }
-                },
-            }
+        Tokenizer {
+            inner: DecodingParser::new(tokenizer, document.encoding()),
         }
     }
 
+    pub(crate) fn feed_code_points(&self, chunk: StrTendril) {
+        self.inner.input_stream().append(chunk);
+    }
+
+    pub(crate) fn feed_bytes(&self, chunk: &[u8]) {
+        self.inner.input_stream().append_bytes(chunk);
+    }
+
     pub(crate) fn end(&self) {
-        self.inner.end()
+        self.inner.sink().end()
     }
 
     pub(crate) fn url(&self) -> &ServoUrl {
-        &self.inner.sink.sink.base_url
+        &self.inner.sink().sink.sink.base_url
+    }
+
+    pub(crate) fn parse(&self) -> impl Iterator<Item = ParserAction<DomRoot<HTMLScriptElement>>> {
+        self.inner.parse().flat_map(|action| {
+            let action = match action {
+                ParserAction::StartOverWithEncoding(encoding) => {
+                    ParserAction::StartOverWithEncoding(encoding)
+                },
+                ParserAction::HandleScript(script) => {
+                    if let Some(script) = script.downcast::<HTMLScriptElement>() {
+                        ParserAction::HandleScript(DomRoot::from_ref(script))
+                    } else {
+                        return None;
+                    }
+                },
+            };
+            Some(action)
+        })
+    }
+
+    pub(crate) fn reset_insertion_point_to_start_of_stream(&self) {
+        self.inner
+            .input_stream()
+            .reset_insertion_point_to_start_of_stream();
+    }
+
+    pub(crate) fn finish_decoding_input(&self) {
+        self.inner.input_stream().finish_decoding_input();
+    }
+
+    pub(crate) fn clear_input_stream(&self) {
+        self.inner.input_stream().clear();
+    }
+
+    pub(crate) fn insert_code_points_at_insertion_point(&self, chunk: StrTendril) {
+        self.inner.input_stream().write_at_insertion_point(chunk);
     }
 }
